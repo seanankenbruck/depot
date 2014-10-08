aaf230000d7c2229d1b92ce2320da4960216fb98
Sean Ankenbruck <smankenb@gmail.com>
2014-10-07 21:36:34 -0400

    smarter, errors logged, total price

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,7 +1,21 @@
 class Cart < ActiveRecord::Base
 	has_many :line_items, dependent: :destroy
-end
-
 #each line_item contains a reference to its cart's id
 #existence of line_item is dependent: on the existence of the cart
 #if a cart is :destroyed Rails will also destroy line_items associated with it 
+
+	def add_product(product_id)
+		current_item = line_items.find_by(product_id: product_id)
+		if current_item
+			current_item.quantity += 1
+		else
+			current_item = line_items.build(product_id: product_id)
+		end
+		current_item
+	end
+
+	def total_price
+		line_items.to_a.sum { |item| item.total_price } #this sums the total price of all line_items 
+	end
+end
+
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,10 @@
 class LineItem < ActiveRecord::Base 
   belongs_to :product #rows in the LineItem model are children of rows in the products table
   belongs_to :cart	  #rows in the LineItem model are children of rows in the carts table
+
+  def total_price
+  	product.price * quantity #this calculates the total price of each line_item
+  end
 end
 
 #for each foreign key (product and cart) this model will have a belongs_to method for each
